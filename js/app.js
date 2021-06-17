354d78fa823a5473fd3fd75bbb4d6ca2df5390d3
kholilboy <kholil.zboy@gmail.com>
2021-06-18 01:58:54 +0700

    update

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ particlesJS('particles-js',
         "value": 100,
         "density": {
           "enable": true,
-          "value_area": 400
+          "value_area": 800
         }
       },
       "color": {
